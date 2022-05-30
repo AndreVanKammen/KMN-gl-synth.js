0e4497440cc343062c807b6c824a6de89a58c539
Andre van Kammen <andrevankammen@gmail.com>
2022-05-30 19:30:13 +0200

    converters for controls

--- webgl-synth-controls.js
+++ webgl-synth-controls.js
@@ -1,27 +1,26 @@
 class ControlRecord {
-  name = '';          // The name of the variable for use in the shaderCode, will be the name of a define which does the call or a const set
-  controlNr = -1;             // The midi-controlnr to get the value from -1 if there is none
-  converterName = '';       // The name of a GLSL converter function, this could also be an envelope generator
-  /** @type {number[]} */
-  converterParams = []; // array of float eg. -70, 6 for dBtoLinear
-
   /**
    * 
-   * @param {string} name 
-   * @param {number} controlNr 
-   * @param {string} converterName 
-   * @param {number[]} converterParams 
+   * @param {string} name The name of the variable for use in the shaderCode, will be the name of a define which does the call or a const set
+   * @param {string} converterName The name of a GLSL converter function, this could also be an envelope generator
+   * @param {number[]} converterParams array of float eg. -70, 6 for dBtoLinear 
    */
-  constructor(name, controlNr, converterName, converterParams) {
+  constructor(name, converterName, converterParams) {
     this.name = name;
-    this.controlNr = controlNr;
     this.converterName = converterName;
     this.converterParams = converterParams;
   }
 }
 class ConverterRecord {
-  name = '';
-  code = '';
+  /**
+   * 
+   * @param {string} name 
+   * @param {string} code 
+   */
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
 }
 
 export class WebGLSynthControls {
@@ -32,25 +31,45 @@ export class WebGLSynthControls {
     this.controls = {};
   }
 
-  setControl(name, controlNr, converterName, converterParams) {
-    this.controls[name] = new ControlRecord(name, controlNr, converterName, converterParams);
+  /**
+   * 
+   * @param {string} name functions name
+   * @param {string} code GLSL function code for converting params to a value can use getControl
+   */
+  setConverter(name, code) {
+    this.converters[name] = new ConverterRecord(name, code);
+  }
+
+  /**
+   * 
+   * @param {string} name The name of the variable for use in the shaderCode, will be the name of a define which does the call or a const set
+   * @param {string} converterName The name of a GLSL converter function, this could also be an envelope generator
+   * @param {number[]} converterParams array of float eg. -70, 6 for dBtoLinear 
+   */
+  setControl(name, converterName, converterParams) {
+    this.controls[name] = new ControlRecord(name, converterName, converterParams);
   }
 
   createDefaultMidiControls() {
-    this.setControl('modulation', 1);
-    this.setControl('expression', 11);
-    this.setControl('volume', 7);
-    this.setControl('pan', 10);
-    this.setControl('program', 128);
-    this.setControl('pitch', 129);
-    this.setControl('pitchRange', 131);
-    this.setControl('pressure', 130);
+    this.setControl('modulation', 'getControl',[1]);
+    this.setControl('expression', 'getControl',[11]);
+    this.setControl('volume', 'getControl',[7]);
+    this.setControl('pan', 'getControl',[10]);
+    this.setControl('program', 'getControl',[128]);
+    this.setControl('pitch', 'getControl',[129]);
+    this.setControl('pitchRange', 'getControl',[131]);
+    this.setControl('pressure', 'getControl',[130]);
   }
 
   getControlsShaderCode(shaderCode) {
-    let resultStr = '';
+    let resultStr = '\n';
+    for (let name of Object.keys(this.converters)) {
+      resultStr += this.converters[name].code + '\n';
+    }
+    resultStr += '\n';
     for (let name of Object.keys(this.controls)) {
-      resultStr += `#define ${name} getControl(${this.controls[name].controlNr})\n`;
+      let cr = this.controls[name];
+      resultStr += `#define ${name} ${cr.converterName}(${cr.converterParams.join(',')})\n`;
     }
     console.log('getControlsShaderCode:', resultStr);
     return resultStr;
